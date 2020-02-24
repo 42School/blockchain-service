6fdf8bbddbfcab3da85f857b3f8dcc8e3372ef44
Louise P <pieri.louisse@gmail.com>
2020-02-24 13:16:19 +0100

    Clean the branch master

--- front/config/prod.env.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  BACK_URL: 'https://localhost:3001'
-}
--- front/src/components/footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <strong>42 Blockchain</strong>.
-      </p>
-    </div>
-  </footer>
-</template>
