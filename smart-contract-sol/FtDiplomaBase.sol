0bcf801540e8da7faba85c81f9c828a3579030ab
Louise P <pieri.louisse@gmail.com>
2020-02-27 13:28:51 +0100

    [Update]: FtDiplomaBase.sol -> create a variable 'ftAddress' and any function for this, change all private function in public function

--- smart-contract-sol/FtDiplomaBase.sol
+++ smart-contract-sol/FtDiplomaBase.sol
@@ -1,10 +1,15 @@
+/*
+**	Autor: Louise Pieri
+*/
 pragma solidity >=0.6.1;
 
 contract	FtDiplomaBase {
 
 	string public constant name = "42 Diploma";
 	string public constant symbol = "42D";
+	address public ftPubAddress;
 
+	event PublishFtAddress(address ftPubAddress);
 	event CreateDiploma(address student, uint256 diplomaId);
 	event Transfer(address sender, address recipient, uint256 diplomaId);
 
@@ -16,22 +21,32 @@ contract	FtDiplomaBase {
 	Diploma[] private diplomas;
 	mapping (address => uint256) private addressToDiploma;
 
-	function _createDiploma(string memory _data, bytes32 _signature, address _studentAddress) private {
+	constructor (address _ftAddress) internal {
+		require(_ftAddress != address(0), "42-Diploma: The _ftAddress is equal to zero.");
+
+		ftPubAddress = _ftAddress;
+		emit PublishFtAddress(ftPubAddress);
+	}
+
+	function changeFtAddress(address sender, address recipient) public {
+		require(sender == ftPubAddress, "42-Diploma: The sender is not equal to the address of 42.");
+		require(sender != address(0), "42-Diploma: The sender is equal to zero.");
+		require(recipient != address(0), "42-Diploma: The sender is equal to zero.");
+
+		ftPubAddress = recipient;
+		emit PublishFtAddress(recipient);
+	}
+
+	function createDiploma(string memory _data, bytes32 _signature, address _studentAddress) public {
 		require(_studentAddress != address(0), "42-Diploma: The student address is equal at 0.");
+
 		diplomas.push(Diploma(_data, _signature));
 		uint256 newID = diplomas.length - 1;
 		addressToDiploma[_studentAddress] = newID;
 		emit CreateDiploma(_studentAddress, newID);
 	}
 
-	function _getDiploma(address _studentAddress) private view returns (string memory data) {
-		require(_studentAddress != address(0), "42-Diploma: The student address is equal at 0.");
-		Diploma memory getDiploma = diplomas[addressToDiploma[_studentAddress]];
-		string memory dataDiploma = getDiploma.data;
-		return (dataDiploma);
-	}
-
-	function _transferDiploma(address sender, address recipient, uint256 diplomaId) private {
+	function transferDiploma(address sender, address recipient, uint256 diplomaId) public {
 		require(sender != address(0), "42-Diploma: Transfer sender the zero address.");
 		require(recipient != address(0), "42-Diploma: Transfer recipient the zero address.");
 		require(addressToDiploma[sender] == diplomaId, "42-Diploma: The address 'sender' is not the owner of the diploma.");
@@ -40,6 +55,18 @@ contract	FtDiplomaBase {
 		addressToDiploma[sender] = 0;
 		addressToDiploma[recipient] = diplomaId;
 		emit Transfer(sender, recipient, diplomaId);
+	}
+
+	function getDiploma(address _studentAddress) public view returns (string memory data) {
+		require(_studentAddress != address(0), "42-Diploma: The student address is equal at 0.");
+
+		Diploma memory _getDiploma = diplomas[addressToDiploma[_studentAddress]];
+		string memory dataDiploma = _getDiploma.data;
+		return (dataDiploma);
+	}
+
+	function getFtAddress() public view returns (address) {
+		return (ftPubAddress);
 	}
 
 }
