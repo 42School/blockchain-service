1fc9c65cf3e9fad78dd9fca0df420a24e1e4f41e
Louise P <cpieri@student.42.fr>
2020-07-14 15:16:47 +0200

    [API]: Add .env with all variable require & use this in the code

--- src/account/account.go
+++ src/account/account.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/lpieri/42-Diploma/src/global"
 	"io/ioutil"
 )
 
-var addr common.Address = common.HexToAddress("0x8A21Dc0aeC762cD85de81B2bcd396a9d5676cFD7")
 var AccountsManager *accounts.Manager
 var KeyStore *keystore.KeyStore
 
@@ -23,7 +21,7 @@ func GetAccount() accounts.Account {
 }
 
 func GetKey() (*keystore.Key, error) {
-	keyjson, errRead := ioutil.ReadFile(global.PathKeyStore + "/UTC--2020-06-29T11-29-15.108416000Z--8a21dc0aec762cd85de81b2bcd396a9d5676cfd7")
+	keyjson, errRead := ioutil.ReadFile(global.PathKeyStore + "/" + global.FileKeyStore)
 	if errRead != nil {
 		return nil, errRead
 	}
@@ -31,8 +29,8 @@ func GetKey() (*keystore.Key, error) {
 	if errDecrypt != nil {
 		return nil, errDecrypt
 	}
-	if key.Address != addr {
-		return nil, fmt.Errorf("key content mismatch: have account %x, want %x", key.Address, addr)
+	if key.Address != global.OfficialAddress {
+		return nil, fmt.Errorf("key content mismatch: have account %x, want %x", key.Address, global.OfficialAddress)
 	}
 	return key, nil
 }
