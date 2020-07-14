100e9216679c3630e97efd25d89434a450dad94f
Louise P <cpieri@student.42.fr>
2020-07-14 14:34:33 +0200

    [Smart-Contract]: Add error if the diploma exist

--- contracts/FtDiploma.sol
+++ contracts/FtDiploma.sol
@@ -4,11 +4,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.8 <0.7.0;
 
-contract	FtDiplomaBase {
+contract	FtDiploma {
 
-	string public constant name = "42 Diploma";
-	string public constant symbol = "42D";
-	string public constant linkOfRepo = "github.com/lpieri/42-Diploma";
+	string public constant name = "42 Alumni";
+	string public constant symbol = "42A";
+	string public constant linkOfRepo = "github.com/lpieri/42-Alumni";
 	address public constant ftPubAddress = 0x8A21Dc0aeC762cD85de81B2bcd396a9d5676cFD7;
 
 	event Publish42Diploma(address ftPubAddress, string _link);
@@ -33,7 +33,8 @@ contract	FtDiplomaBase {
 	}
 
 	function createDiploma(uint64 _level, uint64[30] memory _skills, uint8 _v, bytes32 _r, bytes32 _s, bytes32 _studentHash) public {
-		require(ecrecover(_studentHash, _v, _r, _s) == ftPubAddress, "FtDiplomaBase: Is not 42 sign this diploma");
+		require(ecrecover(_studentHash, _v, _r, _s) == ftPubAddress, "FtDiploma: Is not 42 sign this diploma");
+		require(hashToDiploma[_studentHash].level == 0, "FtDiploma: The diploma already exists.");
 		hashToDiploma[_studentHash] = Diploma(_level, _skills, Sign(_v, _r, _s));
 		emit CreateDiploma(_studentHash);
 	}
