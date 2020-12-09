ebeb14f7d477fff43238f09e69b9aa2981cb2e8e
Louise P <cpieri@student.42.fr>
2020-12-09 12:14:04 +0100

    [Refactor]: Controllers-Diplomas

--- src/api/controllers/diplomas.go
+++ src/api/controllers/diplomas.go
@@ -25,56 +25,54 @@ type ResponseJson struct {
 func CreateDiploma(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json;charset=UTF-8")
 	var newDiploma models.Diploma
-	jsonData, readErr := ioutil.ReadAll(r.Body)
-	jsonErr := json.Unmarshal(jsonData, &newDiploma)
 	if r.Header.Get("Token") != global.Token {
-		res, _ := json.Marshal(ResponseJson{false, "You are not authorized !", ResponseData{}})
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write(res)
+		http.Error(w, "You are not authorized !", http.StatusUnauthorized)
 		return
 	}
-	if r.ContentLength == 0 || readErr != nil || jsonErr != nil || newDiploma.CheckDiploma() == false {
-		res, _ := json.Marshal(ResponseJson{false, "The data sent are not valid, to be written in blockchain please try again!", ResponseData{}})
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(res)
+	err := json.NewDecoder(r.Body).Decode(&newDiploma)
+	if err != nil {
+		http.Error(w, "Fail Unmarshalling json", http.StatusBadRequest)
+		return
+	}
+	if r.ContentLength == 0 || newDiploma.CheckDiploma() == false {
+		http.Error(w, "The data sent are not valid, to be written in blockchain please try again !", http.StatusBadRequest)
 		return
 	}
 	if global.SecuritySystem {
 		newDiploma.AddToRetry()
-		res, _ := json.Marshal(ResponseJson{false, "The security system is activated, the request has just been queued.", ResponseData{}})
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write(res)
+		http.Error(w, "The security system is activated, the request has just been queued.", http.StatusInternalServerError)
 		return
 	}
 	tools.LogsDev("Received request to write the " + newDiploma.FirstName + " " + newDiploma.LastName + " diploma.")
-	var res []byte
 	hash, bool := newDiploma.EthWriting()
 	if bool == false {
-		res, _ = json.Marshal(ResponseJson{false, "Blockchain writing had a problem, the diploma is saved in the queue.", ResponseData{"", 0, []float64{}}})
-		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, "Blockchain writing had a problem, the diploma is saved in the queue.", http.StatusBadRequest)
+		return
 	} else {
-		res, _ = json.Marshal(ResponseJson{true, "The writing in blockchain has been done, it will be confirmed in 10 min.", ResponseData{hash, 0, []float64{}}})
+		res, _ := json.Marshal(ResponseJson{true, "The writing in blockchain has been done, it will be confirmed in 10 min.", ResponseData{hash, 0, []float64{}}})
 		w.WriteHeader(http.StatusOK)
+		w.Write(res)
 	}
-	w.Write(res)
 	return
 }
 
 func GetDiploma(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json;charset=UTF-8")
 	var diploma models.Diploma
-	var res []byte
-	jsonData, readErr := ioutil.ReadAll(r.Body)
-	jsonErr := json.Unmarshal(jsonData, &diploma)
-	level, skills, errGet := diploma.EthGetter()
-	if r.ContentLength == 0 || readErr != nil || jsonErr != nil || errGet != nil {
-		res, _ = json.Marshal(ResponseJson{false, "The request is fail, please retry & check the data", ResponseData{}})
-		w.WriteHeader(http.StatusBadRequest)
-	} else {
-		res, _ = json.Marshal(ResponseJson{true, "", ResponseData{"", level, skills[:]}})
-		w.WriteHeader(http.StatusOK)
+	err := json.NewDecoder(r.Body).Decode(&diploma)
+	if err != nil {
+		http.Error(w, "Fail Unmarshalling json", http.StatusBadRequest)
+		return
 	}
-	w.Write(res)
+	level, skills, errGet := diploma.EthGetter()
+	if r.ContentLength == 0 || errGet != nil {
+		http.Error(w, "The request is fail, please retry & check the data.", http.StatusBadRequest)
+		return
+	} else {
+		res, _ := json.Marshal(ResponseJson{true, "", ResponseData{"", level, skills[:]}})
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+	}
 	return
 }
 
