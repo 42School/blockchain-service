123985c5ea2c3eec8a80262cb16ba3b8a94ca6fe
Louise P <cpieri@student.42.fr>
2021-04-02 17:33:22 +0200

    [Update]: Adding key/val of skills in smart-contract

--- contracts/FtDiploma.sol
+++ contracts/FtDiploma.sol
@@ -10,7 +10,7 @@ contract	FtDiploma {
 	string public constant name = "42 Alumni";
 	string public constant symbol = "42A";
 	string public constant linkOfRepo = "github.com/42School/blockchain-service";
-	address public constant ftPubAddress = ;
+	address public constant ftPubAddress = 0x7e12234E994384A757E2689aDdB2A463ccD3B47d;
 
 	event Publish42Diploma(address ftPubAddress, string _link);
 	event CreateDiploma(bytes32 student);
@@ -21,9 +21,21 @@ contract	FtDiploma {
 		bytes32		s;
 	}
 
+	struct	Skill {
+		string		slug;
+		uint64		level;
+	}
+
+	struct	Diplomas {
+		uint64		level;
+		Skill[30]	skills;
+		bytes32		hash;
+		Sign		signature;
+	}
+
 	struct	Diploma {
 		uint64		level;
-		uint64[30]	skills;
+		mapping(uint64 => Skill) skills;
 		bytes32		hash;
 		Sign		signature;
 	}
@@ -35,26 +47,41 @@ contract	FtDiploma {
 		emit Publish42Diploma(ftPubAddress, linkOfRepo);
 	}
 
-	function createDiploma(uint64 _level, uint64[30] memory _skills, uint8 _v, bytes32 _r, bytes32 _s, bytes32 _studentHash) public {
+	function createDiploma(uint64 _level, uint64[30] memory _skillLevel, string[30] memory _skillSlug, uint8 _v, bytes32 _r, bytes32 _s, bytes32 _studentHash) public {
 		require(ecrecover(_studentHash, _v, _r, _s) == ftPubAddress, "FtDiploma: Is not 42 sign this diploma.");
 		require(hashToDiploma[_studentHash].level == 0, "FtDiploma: The diploma already exists.");
-		hashToDiploma[_studentHash] = Diploma(_level, _skills, _studentHash, Sign(_v, _r, _s));
+		Diploma storage dp = hashToDiploma[_studentHash];
+		dp.level = _level;
+		dp.hash = _studentHash;
+		dp.signature = Sign(_v, _r, _s);
+		for (uint64 i = 0; i < 30; i++) {
+			dp.skills[i] = Skill(_skillSlug[i], _skillLevel[i]);
+		}
 		intToHash.push(_studentHash);
 		emit CreateDiploma(_studentHash);
 	}
 
-	function getDiploma(bytes32 _studentHash) public view returns (uint64 level, uint64[30] memory skills) {
-		Diploma memory _getDiploma = hashToDiploma[_studentHash];
+	function getDiploma(bytes32 _studentHash) public view returns (uint64, Skill[] memory) {
+		Diploma storage _getDiploma = hashToDiploma[_studentHash];
 		uint64 levelDiploma = _getDiploma.level;
-		uint64[30] memory skillsDiploma = _getDiploma.skills;
-		return (levelDiploma, skillsDiploma);
+		Skill[] memory skills = new Skill[](30);
+		for (uint64 i = 0; i < 30; i++) {
+			skills[i] = _getDiploma.skills[i];
+		}
+		return (levelDiploma, skills);
 	}
 
-	function getAllDiploma() public view returns (Diploma[] memory) {
+	function getAllDiploma() public view returns (Diplomas[] memory) {
 		require(msg.sender == ftPubAddress, "FtDiploma: Is not 42.");
-		Diploma[] memory diplomas = new Diploma[](intToHash.length);
+		Diplomas[] memory diplomas = new Diplomas[](intToHash.length);
 		for (uint256 i = 0; i < intToHash.length; i++) {
-			diplomas[i] = hashToDiploma[intToHash[i]];
+			Diploma storage dp = hashToDiploma[intToHash[i]];
+			diplomas[i].level = dp.level;
+			for (uint64 y = 0; i < 30; i++) {
+				diplomas[i].skills[y] = dp.skills[y];
+			}
+			diplomas[i].hash = dp.hash;
+			diplomas[i].signature = dp.signature;
 		}
 		return (diplomas);
 	}
--- test/ft_diploma_test.js
+++ test/ft_diploma_test.js
@@ -2,10 +2,19 @@ const truffleConfig = require("../truffle-config");
 
 const FtDiploma = artifacts.require("FtDiploma");
 
-let skills = [
+let skillsLevel = [
 	857, 542, 62, 942, 661, 416, 902, 902, 902, 36, 222, 55, 145,
 	435, 267, 1122, 73, 206, 103, 817, 116, 2828, 920, 157, 1112,
 	2126, 328, 423, 203, 416
+]
+
+let skillsSlug = [
+	"Written Communication", "Web", "Unix", "Technology integration", "Shell",
+	"Security", "Ruby", "Rigor", "Python", "Parallel computing", "Organization",
+	"Object-oriented programming", "Network & system administration", "Motion Design",
+	"Imperative programming", "Group & interpersonal", "Graphics", "Functional programming",
+	"DB & Data", "Company experience", "Basics", "Algorithms & AI", "Adaptation & creativity",
+	"157", "1112", "2126", "328", "423", "203", "416"
 ]
 
 const keystore = `{"address":"7e12234e994384a757e2689addb2a463ccd3b47d","crypto":{"cipher":"aes-128-ctr","ciphertext":"6d9f85fc277cbbd66f01f15358a62fff3a1cef9c7a96a8024043d8b3c9adeaf6","cipherparams":{"iv":"39516f9f12fe7cccfbdaa11dab569cb9"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"56bb6dac014acbb558e2cb6c533d73d25dc3fe7fa2b1653e664ce32fa72347d5"},"mac":"7d69e74114b8529df420adb173d62a97ef12f631272253c8ce69416c2234245d"},"id":"3ae16ea0-ef89-4b2c-b527-ad1259a53b4d","version":3}`;
@@ -18,7 +27,7 @@ contract("FtDiploma", async (accounts) => {
 		let dataToHash = "Louise, Pieri, 1998-12-27, 2020-06-25";
 		let hash = web3.utils.sha3(dataToHash);
 		let sign = account.sign(hash);
-		const tx = await instance.createDiploma(1517, skills, sign.v, sign.r, sign.s, sign.messageHash);
+		const tx = await instance.createDiploma(1517, skillsLevel, skillsSlug, sign.v, sign.r, sign.s, sign.messageHash);
 		txStudent = tx.logs[0].args.student;
 		assert.equal(txStudent, sign.messageHash, 'Error: The writing is not valid!');
 	})
@@ -29,7 +38,7 @@ contract("FtDiploma", async (accounts) => {
 		let hash = web3.utils.sha3(dataToHash);
 		let sign = account.sign(hash);
 		const tx = await instance.getDiploma(sign.messageHash);
-		level = web3.utils.BN(tx.level).toNumber();
+		level = web3.utils.BN(tx[0]).toNumber();
 		assert.equal(level, 1517, `Error: The diploma couldn't be gotten, or doesn't exist.`);
 	})
 
@@ -39,7 +48,7 @@ contract("FtDiploma", async (accounts) => {
 		let hash = web3.utils.sha3(dataToHash);
 		let sign = account.sign(hash);
 		try {
-			await instance.createDiploma(1517, skills, sign.v, sign.r, sign.s, hash);
+			await instance.createDiploma(1517, skillsLevel, skillsSlug, sign.v, sign.r, sign.s, hash);
 		} catch (error) {
 			assert.equal(error.reason, 'FtDiploma: Is not 42 sign this diploma.', `Error: It is possible to write a diploma not signed by 42.`);
 		}
@@ -50,9 +59,9 @@ contract("FtDiploma", async (accounts) => {
 		let dataToHash = "Prenom, Nom, AAAA-MM-JJ, AAAA-MM-JJ";
 		let hash = web3.utils.sha3(dataToHash);
 		let sign = account.sign(hash);
-		const firstTx = await instance.createDiploma(1517, skills, sign.v, sign.r, sign.s, sign.messageHash);
+		const firstTx = await instance.createDiploma(1517, skillsLevel, skillsSlug, sign.v, sign.r, sign.s, sign.messageHash);
 		try {
-			await instance.createDiploma(1517, skills, sign.v, sign.r, sign.s, sign.messageHash);
+			await instance.createDiploma(1517, skillsLevel, skillsSlug, sign.v, sign.r, sign.s, sign.messageHash);
 		} catch (error) {
 			assert.equal(error.reason, "FtDiploma: The diploma already exists.", `Error: The double insertion of a diploma is possible.`);
 		}
@@ -70,6 +79,7 @@ contract("FtDiploma", async (accounts) => {
 	it("Testing the gets all data", async () => {
 		let instance = await FtDiploma.deployed();
 		let datas = await instance.getAllDiploma({from: "0x7e12234e994384a757e2689addb2a463ccd3b47d"})
+		console.log(datas);
 		assert.isNotEmpty(datas);
 	})
 })
